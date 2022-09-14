bdd4699a93591cf5a6eb0393aaa9a59482aee59b
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-14 15:08:40 -0300

    :sparkles: feat: add create user use case and tests

---
+++ src/usecases/create-user.spec.ts
@@ -0,0 +1,30 @@
+import { it, describe, expect } from 'vitest';
+import { CreateUserUseCase } from './create-user';
+import { InMemoryUsersRepository } from '../repositories/implementations/in-memory-users-repository';
+import { User } from '../entities/user';
+
+describe('Create User', () => {
+  it('should be able to create an user', async () => {
+    const createUserUseCase = new CreateUserUseCase(
+      new InMemoryUsersRepository(),
+    );
+
+    expect(createUserUseCase.execute({
+      name: 'John Doe',
+    })).resolves.toBeInstanceOf(User);
+  });
+
+  it('should not be able to create an user', async () => {
+    const createUserUseCase = new CreateUserUseCase(
+      new InMemoryUsersRepository(),
+    );
+
+    await createUserUseCase.execute({
+      name: 'John Doe',
+    });
+
+    expect(createUserUseCase.execute({
+      name: 'John Doe',
+    })).rejects.toThrow();
+  });
+});
---
+++ src/usecases/create-user.ts
@@ -0,0 +1,30 @@
+import { User } from '../entities/user';
+import { UsersRepository } from '../repositories/users-repository';
+
+interface CreateUserRequest {
+  name: string;
+}
+
+type CreateUserResponse = User;
+
+export class CreateUserUseCase {
+  constructor(
+    private usersRepository: UsersRepository,
+  ) {}
+
+  async execute({ name }: CreateUserRequest): Promise<CreateUserResponse> {
+    const userAlreadyExists = await this.usersRepository.findByName(name);
+
+    if (userAlreadyExists) {
+      throw new Error(`User ${name} already exists`);
+    }
+
+    const user = new User({
+      name,
+    });
+
+    await this.usersRepository.create(user);
+
+    return user;
+  }
+}
