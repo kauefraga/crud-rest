c175cbcea45d34ae0ed92f3eccae18153f1e1775
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-15 22:05:29 -0300

    :sparkles: feat: init `src/http`

---
+++ src/http/app.ts
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import express, { Application } from 'express';
+import { router } from './routes';
+
+function buildApp(): Application {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.use(router);
+
+  return app;
+}
+
+export default buildApp();
