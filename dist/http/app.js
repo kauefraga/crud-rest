0872de03fafc36d45c36289f8df69f3416fe855f
kauefraga <kauefragarodrigues456@gmail.com>
2022-09-15 23:51:08 -0300

    :boom: feat: build middlewares

--- dist/http/app.js
+++ dist/http/app.js
@@ -3,14 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const RequestLimiter_1 = require("./middlewares/RequestLimiter");
 const routes_1 = require("./routes");
+const _404_1 = require("./middlewares/404");
 function buildApp() {
     const app = (0, express_1.default)();
     app.use((0, cors_1.default)());
     app.use(express_1.default.json());
+    app.use(RequestLimiter_1.RequestLimiter);
     app.use(routes_1.router);
+    app.use(_404_1.NotFound);
     return app;
 }
 exports.default = buildApp();
