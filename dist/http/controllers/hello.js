eb2eb6419ac0ff11753c3a9143fce22c4b07e263
kauefraga <kauefragarodrigues456@gmail.com>
2022-10-27 20:15:09 -0300

    :see_no_evil: ignores: include tsc output

--- dist/http/controllers/hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (_, response) => response.status(200).json({
-    success: true,
-    data: {
-        message: 'Hello, world! 👋',
-        now: new Date(),
-    },
-});
